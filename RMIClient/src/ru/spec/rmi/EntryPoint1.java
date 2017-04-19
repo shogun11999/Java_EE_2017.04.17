b3751dbe3ddb5fb30b2260deba98eb351f39fb9e
User <User@tg_-__>
2017-04-19 18:54:42 +0300

    2

--- RMIClient/src/ru/spec/rmi/EntryPoint1.java
+++ RMIClient/src/ru/spec/rmi/EntryPoint1.java
@@ -11,7 +11,7 @@ import ru.spec.ee.ejb.IEchoService;
 public class EntryPoint1 {
 
 	public static void main(String[] args) throws NamingException {
-		// TODO Auto-generated method stub
+		
 		Context ctx = new InitialContext();
 		IEchoService echoBean = (IEchoService) ctx.lookup("echoBean");
 		String result = echoBean.echo("hello");
